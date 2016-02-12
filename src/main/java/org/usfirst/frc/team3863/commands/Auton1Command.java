2ececf04cc55cdbe05626de46f88c60d819d9630
Joshua Freedman <npjoshf@gmail.com>
2016-02-12 15:34:53 -0800

    I am an idiot... How do I screw up | or ||.

---
+++ src/main/java/org/usfirst/frc/team3863/commands/Auton1Command.java
@@ -0,0 +1,12 @@
+package org.usfirst.frc.team3863.commands;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ * Created by Robotics on 2/12/2016.
+ */
+public class Auton1Command extends CommandGroup {
+
+    Auton1Command() {
+    }
+}
